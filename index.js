5344ca386ea5b87004442638472a3fd5258c3606
Ashfakul Islam <r4pido3@gmail.com>
2023-12-15 19:05:48 +0600

    user information instering in db

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(express.json());
 app.use(cors());
 
-// MONGODB CONFIG
+///////////////// MONGODB CONFIG//////////////////////
 
 const uri = process.env.MONGO_URI;
 
@@ -18,36 +18,53 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    //////////////////////DB Collections////////////
+    const database = client.db("bloodUnity");
+    const users = database.collection("users");
+
+    ///////////////////////// USER COLLECTION////////////////////////
+
+    app.post("/users", async (req, res) => {
+      try {
+        const userinfo = req.body;
+        const result = await users.insertOne(userinfo);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
 run().catch(console.dir);
 
+//////////////////////// NORMAL ROUTES/////////////////
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to Blood Unity" });
+});
 
-// NORMAL ROUTES
-app.get('/', (req, res) => {
-    res.send({message: 'Welcome to Blood Unity'})
-})
-
-app.all('*', (req,res,next) => {
-    const error = new Error (`The Requested URL is invalid: ${req.url}`)
-    error.status = 404;
-    next(error)
-})
+app.all("*", (req, res, next) => {
+  const error = new Error(`The Requested URL is invalid: ${req.url}`);
+  error.status = 404;
+  next(error);
+});
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({message: err.message})
-})
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
-})
+  console.log(`listening on ${port}`);
+});
